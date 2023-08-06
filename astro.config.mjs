ab1f2721ffce21f6de1150e8d44dcd23edb24176
Anton Savostianov <a.s.savostyanov@gmail.com>
2023-08-06 21:21:49 +0200

    small edits

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 
+import netlify from "@astrojs/netlify/functions";
+
 // https://astro.build/config
 export default defineConfig({
-      vite: {
-            ssr: {
-                  external: ["svgo"],
-            },
-      },
+  vite: {
+    ssr: {
+      external: ["svgo"]
+    }
+  },
+  output: "server",
+  adapter: netlify()
 });
